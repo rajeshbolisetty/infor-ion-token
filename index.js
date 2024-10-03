cb45e795fcd0d42903a39300e91efb10f651354f
Rajesh Kumar Bolisetty <rajesh.bnv@gmail.com>
2024-10-03 21:46:18 +1300

    add refresh token method

--- index.js
+++ index.js
@@ -24,6 +24,30 @@ async function getIonToken(clientId, secret, username, password, url) {
   }
 }
 
+async function refreshToken(url, clientId, secret, refreshToken) {
+  try {
+    const { data } = await axios.post(
+      url,
+      new URLSearchParams({
+        grant_type: "refresh_token",
+        client_id: clientId,
+        client_secret: secret,
+        refresh_token: refreshToken,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    throw new Error("Failed to obtain refresh token");
+  }
+}
+
 module.exports = {
   getIonToken,
+  refreshToken,
 };
